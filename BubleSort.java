2322b86e9af4d7e5dc39d365e7756e1a515cb514
Ballcomz <ballcomz@gmail.com>
2020-09-23 15:50:34 +0700

    lab 9 for loop

---
+++ BubleSort.java
@@ -0,0 +1,18 @@
+public class BubleSort{
+    public static void main(String[] args) {
+        int num1,num2,num3,num4,num5;
+        //random number
+        num1 = (int)(Math.random() * 100);
+        num2 = (int)(Math.random() * 100);
+        num3 = (int)(Math.random() * 100);
+        num4 = (int)(Math.random() * 100);
+        num5 = (int)(Math.random() * 100);
+        //Show number before random
+        System.out.println("Random number");
+        System.out.print(num1+" "+num2+" "+num3+" "+num4+" "+num5);
+
+        
+
+
+    }
+} 
---
+++ Matrix.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Matrix{
+    public static void main(String[] args) {
+        int a11,a12,a13,a21,a22,a23,a31,a32,a33;
+        
+        Scanner input = new Scanner(System.in);
+
+        
+
+    }
+}
