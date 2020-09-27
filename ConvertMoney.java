99e70139224d21c6894e201da8b5bae395ff886b
Ballcomz <ballcomz@gmail.com>
2020-09-27 15:38:20 +0700

    Update ConvertMoney.java
    add number

--- ConvertMoney.java
+++ ConvertMoney.java
@@ -6,7 +6,7 @@ public class ConvertMoney {
         System.out.print("Enter US Dollar: ");
         dollar = input.nextDouble();
         bath = dollar*31.8245;
-
+    //1234567899
         System.out.println("Money "+dollar+" Dollar is "+bath+" Bath");
 
     }
